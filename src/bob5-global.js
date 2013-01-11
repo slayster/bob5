87efbb620636550ac3661614d61d8f98d00d7e45
Brendan Ellis <brendan@geniedataservices.co.uk>
2013-01-11 00:22:34 +0000

    File containg global variables and settings

---
+++ src/bob5-global.js
@@ -0,0 +1,91 @@
+/**
+ * File containing globabl variables and settings for the BOB5 Engine
+ * 
+ * @author Brendan Ellis <brendan@slayweb.com>
+ * @package bob5_core
+ */    
+
+/**
+ * Width of the HTML5 Canvas
+ * @var integer
+ */  
+var CANVAS_WIDTH;
+/**
+ * Height of the HTML5 Canvas
+ * @var integer
+ */  
+var CANVAS_HEIGHT;
+
+/**
+ * Delay used in the Renderer loop
+ * @var integer
+ */  
+var RENDER_SPEED = 1;
+/**
+ * Delay used in the Movement loop
+ * @var integer
+ */  
+var MOVEMENT_SPEED = 2;
+/**
+ * Delay used in the Animation loop
+ * @var integer
+ */  
+var ANIMATION_SPEED = 2;
+
+var bob5Display;
+var bob5Canvas;
+var bob5BackBuffer;
+
+var renderer;
+var anicontroller;
+var movecontroller;
+
+/**
+ * Show the Frames Per Second counter or not?
+ * @var boolean
+ */  
+var showFPS = false;
+/**
+ * The number of frames rendered in the current second
+ * @var integer
+ */  
+var frameCounter;
+
+/**
+ * Is a custom cursor being used?
+ * @var boolean
+ */  
+var customCursor = false;
+/**
+ * Object representing the current custom cursor if one is set
+ * @var object
+ */  
+var customCursorObject;
+
+/**
+ * Array containing all current renderable objects
+ * @var array
+ */  
+var renderableObjects;
+/**
+ * Array containing all current animating objects
+ * @var array
+ */  
+var animatingObjects;
+/**
+ * Array containing all current moving objects
+ * @var array
+ */  
+var movingObjects;
+
+var hoverMonitor;
+var hoverObjects;
+
+var mouseDownMonitor;
+var mouseDownObjects;
+
+var mouseUpMonitor;
+var mouseUpObjects;
+
+var mouseClickMonitor;
+var mouseClickObjects;
